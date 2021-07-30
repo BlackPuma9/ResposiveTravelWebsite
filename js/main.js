8a6f849467cf66d914bd8f9d5486d658eeb446d7
Yuliia Yavorska <luba-09@ukr.net>
2021-07-30 19:07:48 +0300

    added video and scroll bar,arrow

--- js/main.js
+++ js/main.js
@@ -54,12 +54,67 @@ window.addEventListener('scroll', scrollHeader)
     });
 
 /*==================== VIDEO ====================*/
+const videoFile = document.getElementById('video-file'),
+	videoButton = document.getElementById('video-button'),
+	videoIcon = document.getElementById('video-icon')
+	
+function playPause(){
+	if(videoFile.paused){
+		//Play video
+		videoFile.play()
+		
+		//We change the icon
+		videoIcon.classList.add('ri-pause-line')
+		videoIcon.classList.remove('ri-play-line')
+	}else{
+		//Pause video
+		videoFile.pause()
+		
+		//We change the icon
+		videoIcon.classList.remove('ri-pause-line')
+		videoIcon.classList.add('ri-play-line')
+	}
+}
+
+videoButton.addEventListener('click',playPause)
+
+function finalVideo(){
+	//Video ends, icon change
+	videoIcon.classList.remove('ri-pause-line')
+	videoIcon.classList.add('ri-play-line')
+}
+
+//ended, whe the video ends
+videoFile.addEventListener('ended', finalVideo)
 
 /*==================== SHOW SCROLL UP ====================*/ 
+function scrollUp(){
+    const scrollUp = document.getElementById('scroll-up');
+    // When the scroll is higher than 200 viewport height, add the show-scroll class to the a tag with the scroll-top class
+    if(this.scrollY >= 200) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
 
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+const sections = document.querySelectorAll('section[id]')
 
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
 
 /*==================== SCROLL REVEAL ANIMATION ====================*/
 
